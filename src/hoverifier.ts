0653dac036465f166361d8464fe0c42fc7ea8dbb
Isaac Snow <isaacjsnow@gmail.com>
2018-07-08 12:30:07 -0700

    refactor: jump targets have positions too

--- src/hoverifier.ts
+++ src/hoverifier.ts
@@ -260,10 +260,7 @@ export const createHoverifier = ({
             // When the mouse stopped for TOOLTIP_DISPLAY_DELAY, show tooltip
             // Don't use mouseover for this because it is only fired once per token,
             // not continuously while moving the mouse
-            allCodeMouseMoves.pipe(
-                debounceTime(TOOLTIP_DISPLAY_DELAY),
-                map(() => false)
-            )
+            allCodeMouseMoves.pipe(debounceTime(TOOLTIP_DISPLAY_DELAY), map(() => false))
         ).subscribe(mouseIsMoving => {
             container.update({ mouseIsMoving })
         })
@@ -301,6 +298,7 @@ export const createHoverifier = ({
         // Only use line and character for comparison
         map(({ position: { line, character }, ...rest }) => ({
             position: { line, character },
+            eventType: 'locationchange',
             ...rest,
         })),
         // Ignore same values
@@ -312,14 +310,14 @@ export const createHoverifier = ({
         map(({ position, codeElement, ...rest }) => {
             const row = getRowInCodeElement(codeElement, position.line)
             if (!row) {
-                return { target: undefined, codeElement, ...rest }
+                return { target: undefined, codeElement, position, ...rest }
             }
             const cell = row.cells[1]!
             const target = findElementWithOffset(cell, position.character)
             if (!target) {
                 console.warn('Could not find target for position in file', position)
             }
-            return { target, codeElement, ...rest }
+            return { target, codeElement, position, ...rest }
         }),
         filter(propertyIsDefined('target'))
     )
@@ -391,14 +389,9 @@ export const createHoverifier = ({
             // 1. Reset the hover content, so no old hover content is displayed at the new position while fetching
             // 2. Show a loader if the hover fetch hasn't returned after 100ms
             // 3. Show the hover once it returned
-            return merge(
-                [undefined],
-                of(LOADING).pipe(
-                    delay(LOADER_DELAY),
-                    takeUntil(hoverFetch)
-                ),
-                hoverFetch
-            ).pipe(map(hoverOrError => ({ hoverOrError, codeElement })))
+            return merge([undefined], of(LOADING).pipe(delay(LOADER_DELAY), takeUntil(hoverFetch)), hoverFetch).pipe(
+                map(hoverOrError => ({ hoverOrError, codeElement }))
+            )
         }),
         share()
     )
