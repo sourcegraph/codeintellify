da4afa6bf81e1ff6c03d785c2392b31b5230524f
Felix Becker <felix.b@outlook.com>
2018-07-09 15:24:22 -0700

    refactor: use implicit return in findPositionsFromEvents
    This operator doesn't need all the local variables,
    they are all just used once. (The share() is unneeded too)

--- src/positions.ts
+++ src/positions.ts
@@ -26,64 +26,47 @@ export interface PositionEvent {
     codeElement: HTMLElement
 }
 
-export const findPositionsFromEvents = () => (elements: Observable<HTMLElement>): Observable<PositionEvent> => {
-    const allMouseOvers = elements.pipe(
-        switchMap(element => fromEvent<MouseEvent>(element, 'mouseover')),
-        map(event => ({ event, eventType: 'mouseover' as SupportedMouseEvent }))
-    )
-    const allClicks = elements.pipe(
-        switchMap(element => fromEvent<MouseEvent>(element, 'click')),
-        map(event => ({ event, eventType: 'click' as SupportedMouseEvent }))
-    )
-
-    const codeMouseOvers = allMouseOvers.pipe(
-        filter(({ event }) => event.currentTarget !== null),
-        map(({ event, ...rest }) => ({
-            event,
-            target: event.target as HTMLElement,
-            codeElement: event.currentTarget as HTMLElement,
-            ...rest,
-        })),
-        // SIDE EFFECT (but idempotent)
-        // If not done for this cell, wrap the tokens in this cell to enable finding the precise positioning.
-        // This may be possible in other ways (looking at mouse position and rendering characters), but it works
-        tap(({ target, codeElement }) => {
-            const td = getTableDataCell(target, codeElement)
-            if (td !== undefined) {
-                convertCodeCellIdempotent(td)
-            }
-        })
-    )
-
-    /**
-     * click events on the code element, ignoring click events caused by the user selecting text.
-     * Selecting text should not mess with the hover, hover pinning nor the URL.
-     */
-    const codeClicksWithoutSelections = allClicks.pipe(filter(() => window.getSelection().toString() === ''))
-
-    const codeClickTargets = codeClicksWithoutSelections.pipe(
-        filter(({ event }) => event.currentTarget !== null),
-        map(({ event, ...rest }) => ({
-            event,
-            target: event.target as HTMLElement,
-            codeElement: event.currentTarget as HTMLElement,
-            ...rest,
-        })),
-        share()
-    )
-
-    return merge(
-        // Should unpin the tooltip even if hover cames back non-empty
-        codeMouseOvers,
-        // Should pin the tooltip if hover cames back non-empty
-        codeClickTargets
+export const findPositionsFromEvents = () => (elements: Observable<HTMLElement>): Observable<PositionEvent> =>
+    merge(
+        elements.pipe(
+            switchMap(element => fromEvent<MouseEvent>(element, 'mouseover')),
+            map(event => ({ event, eventType: 'mouseover' as 'mouseover' })),
+            filter(({ event }) => event.currentTarget !== null),
+            map(({ event, ...rest }) => ({
+                event,
+                target: event.target as HTMLElement,
+                codeElement: event.currentTarget as HTMLElement,
+                ...rest,
+            })),
+            // SIDE EFFECT (but idempotent)
+            // If not done for this cell, wrap the tokens in this cell to enable finding the precise positioning.
+            // This may be possible in other ways (looking at mouse position and rendering characters), but it works
+            tap(({ target, codeElement }) => {
+                const td = getTableDataCell(target, codeElement)
+                if (td !== undefined) {
+                    convertCodeCellIdempotent(td)
+                }
+            })
+        ),
+        elements.pipe(
+            switchMap(element => fromEvent<MouseEvent>(element, 'click')),
+            map(event => ({ event, eventType: 'click' as 'click' })),
+            // ignore click events caused by the user selecting text.
+            // Selecting text should not mess with the hover, hover pinning nor the URL.
+            filter(() => window.getSelection().toString() === ''),
+            filter(({ event }) => event.currentTarget !== null),
+            map(({ event, ...rest }) => ({
+                event,
+                target: event.target as HTMLElement,
+                codeElement: event.currentTarget as HTMLElement,
+                ...rest,
+            }))
+        )
     ).pipe(
         // Find out the position that was hovered over
         map(({ target, codeElement, ...rest }) => {
             const hoveredToken = locateTarget(target, codeElement, false)
             const position = Position.is(hoveredToken) ? { ...hoveredToken } : undefined
             return { position, codeElement, ...rest }
-        }),
-        share()
+        })
     )
-}
